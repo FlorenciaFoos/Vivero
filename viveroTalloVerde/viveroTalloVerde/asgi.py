74ec79f83774d29356d83fe38168d96f2079809c
unknown <flor20_05@hotmail.com>
2021-02-20 13:06:17 -0300

    initial setup

---
+++ viveroTalloVerde/viveroTalloVerde/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for viveroTalloVerde project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viveroTalloVerde.settings')
+
+application = get_asgi_application()
