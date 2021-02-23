821f2bfd9c3e7a455958ed543ce8fb6ea9e027bf
unknown <flor20_05@hotmail.com>
2021-02-23 20:20:08 -0300

    settings db

--- viveroTalloVerde/settings.py
+++ viveroTalloVerde/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'viveroTalloVerde.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vivero_db',
-        'USER': 'postgres',
-        'PASSWORD': 'welcome1',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'vivero_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'welcome1',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 #para heroku
 db_from_env = dj_database_url.config(conn_max_age=600)
